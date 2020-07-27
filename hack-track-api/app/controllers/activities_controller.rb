0e5f15da4bb7c58362c4c3b588dd6a8a7052b05c
aschonely83 <aschonely@gmail.com>
2020-07-27 16:38:15 -0400

    added index and show methods

--- hack-track-api/app/controllers/activities_controller.rb
+++ hack-track-api/app/controllers/activities_controller.rb
@@ -1,2 +1,15 @@
 class ActivitiesController < ApplicationController
+  
+  def index
+    activities = Activity.all 
+    render json: ActivitySerializer.new(activities)
+  end
+    
+  def show
+    activity = Activity.find(params[:id])
+    options = {
+      include: [:player, :course]
+    }
+    render json: ActivitySerializer.new(activity, options)
+  end
 end
