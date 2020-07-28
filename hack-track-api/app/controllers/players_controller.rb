9dbcb0f362b92b9c7d4661b1cb075210778c5fa3
aschonely83 <aschonely@gmail.com>
2020-07-28 17:58:14 -0400

    fixed typo

--- hack-track-api/app/controllers/players_controller.rb
+++ hack-track-api/app/controllers/players_controller.rb
@@ -8,7 +8,7 @@ class PlayersController < ApplicationController
   def create
     player = Player.new(player_params)
     if player.save
-      render json: player, status :accepted
+      render json: player, status: :accepted
     else
         render json: {errors: player.errors.full_message}, status: :unprocessible_entity
     end
