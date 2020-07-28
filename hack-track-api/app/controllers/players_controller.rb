6d00bbbba2c4201ba60021bd06eb0ce5889ae8ed
aschonely83 <aschonely@gmail.com>
2020-07-28 17:30:35 -0400

    added create method

--- hack-track-api/app/controllers/players_controller.rb
+++ hack-track-api/app/controllers/players_controller.rb
@@ -3,5 +3,20 @@ class PlayersController < ApplicationController
     def index
     players = Player.all 
     render json: PlayerSerializer.new(players) 
-  end       
+  end
+
+  def create
+    player = Player.new(player_params)
+    if player.save
+      render json: player, status :accepted
+    else
+        render json: {errors: player.errors.full_message}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name)
+  end
 end
