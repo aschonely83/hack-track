45b855966310905d0afddc8b6b675ac063cd5373
aschonely83 <aschonely@gmail.com>
2020-07-31 15:09:35 -0400

    add create method

--- hack-track-api/app/controllers/activities_controller.rb
+++ hack-track-api/app/controllers/activities_controller.rb
@@ -9,4 +9,19 @@ class ActivitiesController < ApplicationController
     activity = Activity.find(params[:id])
     render json: ActivitySerializer.new(activity)
   end
+
+  def create
+    activity = Activity.new(activity_params)
+    if activity.save
+      render json: ActivitySerializer.new(activity), status: :accepted
+    else
+      render json: {errors: activity.errors.full_messages}, status: :unprocessible_entity 
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:tee_time, :hole_numb, :tee_marker, :par, :score, :player_id, :course_id)
+  end
 end
