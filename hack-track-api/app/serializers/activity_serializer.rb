289a7450138052e9110396637525cb765990ad9d
aschonely83 <aschonely@gmail.com>
2020-07-31 15:09:23 -0400

    updated activity serializer

--- hack-track-api/app/serializers/activity_serializer.rb
+++ hack-track-api/app/serializers/activity_serializer.rb
@@ -1,6 +1,6 @@
 class ActivitySerializer
   include FastJsonapi::ObjectSerializer
-  attributes :tee_time, :hole_numb, :tee_marker, :par, :score
-  belongs_to :player
-  belongs_to :course  
+  attributes :tee_time, :hole_numb, :tee_marker, :par, :score, :player_id, :player, :course_id, :course
+  #belongs_to :player
+  #belongs_to :course  
 end
