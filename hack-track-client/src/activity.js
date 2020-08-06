265f67dea8377848efbe214db2e2ecd21778deeb
aschonely83 <aschonely@gmail.com>
2020-08-06 18:28:23 -0400

    added activity class

---
+++ hack-track-client/src/activity.js
@@ -0,0 +1,15 @@
+class Activity {
+ 
+  constructor(data) {
+    this.tee_time = data.tee_time
+    this.hole_numb = data.hole_numb    
+    this.tee_marker = data.tee_marker
+    this.par = data.par
+    this.score = data.score
+    this .course_id = data.course_id
+    this.player_id = data.player_id
+    Activity.call.push(this)
+  }    
+}
+
+Activity.all = []
