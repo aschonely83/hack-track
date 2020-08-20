1505b5b5439f4fb01960ad8725d163cc70ae28ad
aschonely83 <aschonely@gmail.com>
2020-08-20 19:13:19 -0400

    update create method

--- hack-track-api/app/controllers/activities_controller.rb
+++ hack-track-api/app/controllers/activities_controller.rb
@@ -10,20 +10,15 @@ class ActivitiesController < ApplicationController
     render json: ActivitySerializer.new(activity)
   end
 
-  #def create
-  #  if params[:course_id]
-  #    course = Course.find(params[:course_id])
-  #    activity = course.activities.build(activity_params)
-  #    #binding.pry
-  #    if activity.save
-  #      render json: ActivitySerializer.new(activity)
-  #    end
-  #  end
-  #end
-
-  #private
-  #
-  #def activity_params
-  #  params.permit(:tee_time, :hole_numb, :tee_marker, :par, :score, :course_id)
-  #end
+  def create
+    activity = Activity.new(activity_params)
+    if activity.save
+      render json: ActivitySerializer.new(activity)
+    end
+  end
+  private
+  
+  def activity_params
+    params.require(:activity).permit(:hole_numb, :score, :course_id)
+  end
 end
