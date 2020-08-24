b809ad821e53639839f7ad7505248191dd7b1ea8
aschonely83 <aschonely@gmail.com>
2020-08-23 20:29:46 -0400

    started POST request for activity

--- hack-track-client/src/activity.js
+++ hack-track-client/src/activity.js
@@ -4,6 +4,7 @@ class Activity {
     this.id = activity.id
     this.hole_numb = activityAttributes.hole_numb      
     this.score = activityAttributes.score
+    this.course_id = activityAttributes.course_id
     this.course = activityAttributes.course
     Activity.all.push(this)
     //console.log(this)
@@ -12,13 +13,18 @@ class Activity {
   renderActivityCard() {
     return `
     <div class="activity-card">
-      <div>
-        <div class="hole-card">
-          <h4 class="course-name">Course: ${this.course.name}</h4>
-          <p class="hole-number">#: ${this.hole_numb}</p>
-          <p class="hole-score">Score: ${this.score}</p>
-        </div>
-      </div>  
+      <div class="hole-card" data-id="${this.course.id}">
+        <h4 class="course-name">Course: ${this.course.name}</h4>
+        <p class="hole-number">#: ${this.hole_numb}</p>
+        <p class="hole-score">Score: ${this.score}</p>
+        <form id="activity-form">
+          <label for="name">Hole</label>
+          <input type="text" id="hole-input" name="hole">
+          <label for="name">Score</label>
+          <input type="text" id="score-input" name="score">
+          <input type="submit" id="${this.course.id}" value="Add" class="hole-btn">
+        </form>
+      </div>
     </div>
     `
   }
--- hack-track-client/src/index.js
+++ hack-track-client/src/index.js
@@ -4,9 +4,7 @@ ACTIVITY_URL = `${BASE_URL}/activities`
 document.addEventListener("DOMContentLoaded", () => {
  
   getActivity();
-  
-  
-
+    
 });
 
 function getActivity() {
@@ -14,18 +12,47 @@ function getActivity() {
   .then(resp => resp.json())
   .then(activities => {
     activities.data.forEach(activity => {
-      //debugger
-
+      
       let newActivity = new Activity(activity, activity.attributes)
     
       document.querySelector('.activity-container').innerHTML += newActivity.renderActivityCard()
 
-      const addActivity = document.querySelector(".activity-form")
-      addActivity.addEventListener("submit", function(event) {
-        event.preventDefault();
-        debugger
-      })
+      const addActivity = document.getElementById("activity-form")
+      addActivity.addEventListener("submit", (e) => createForm(e))
+
     })
+  })
+}
+
+function createForm(e) {
+  e.preventDefault()
+  
+  const formData = document.getElementById("activity-form")
+  const holeInput = formData.attributes[0].ownerElement[0].value
+  const scoreInput = formData.attributes[0].ownerElement[1].value
+  const courseId = formData.attributes[0].ownerElement[2].id
+  
+  const activityData = {
+    hole: holeInput,
+    score: scoreInput,
+    course_id: courseId
+  }
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(activityData)
+  }
+
+  fetch(ACTIVITY_URL, configObj)
+  .then(function(resp) {
+    return resp.json()
+  })
+  .then(function(activityData) {
+    let newActivity = new Activity(activityData)
+    renderActivityCard(newActivity, newActivity.course_id)
   })
 }
 
